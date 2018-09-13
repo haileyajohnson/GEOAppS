b34be07658893a8a72bfc6a818d5ad36f6304a4b
Johnson <hailey.johnson@ufl.edu>
2018-09-13 16:53:24 -0400

    reorganize; finish splash page

--- app_class.m
+++ app_class.m
@@ -7,14 +7,20 @@ classdef app_class
         Name
         Description
         Authors
+        Script
+        ImgSrc
+        Link
     end
 
     methods
-        function obj = app_class(title, name, description, authors)
+        function obj = app_class(title, name, description, authors, script, ImgSrc, Link)
             obj.Title = title;
             obj.Name = name;
             obj.Description = description;
             obj.Authors = authors;
+            obj.Script = script;
+            obj.ImgSrc = ImgSrc;
+            obj.Link = Link;
         end
     end
 end
