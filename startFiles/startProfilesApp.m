44f3c43a4f8096b632eff819cf7ef415d82c9ee5
Johnson <hailey.johnson@ufl.edu>
2020-03-12 15:27:57 -0400

    reorganize repository

--- startFiles/startProfilesApp.m
+++
@@ -1,6 +0,0 @@
-fileId = '1ASkChtWxILyok3xMO70mTUhANgrzAHWL';
-url = ['https://drive.google.com/uc?export=download&id=', fileId];
-
-appFile = websave('The Bruun Rule.mlappinstall', url);
-app = matlab.apputil.install(appFile);
-matlab.apputil.run(app.id);
--- startFiles/startRefractionApp.m
+++
@@ -1,6 +0,0 @@
-fileId = '1ZfQeiOZFj7V6VISRxJXiNkpREH8BHTjk';
-url = ['https://drive.google.com/uc?export=download&id=', fileId];
-
-appFile = websave('Refraction.mlappinstall', url);
-app = matlab.apputil.install(appFile);
-matlab.apputil.run(app.id);
